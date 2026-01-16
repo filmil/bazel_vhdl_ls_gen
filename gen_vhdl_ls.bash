7326035c4c61d4299cbd564715950eb089aa5ebe
Filip Filmar <filmil@gmail.com>
2026-01-16 01:59:01 -0800

    feat: add prefix and suffix files

--- gen_vhdl_ls.bash
+++ gen_vhdl_ls.bash
@@ -4,6 +4,8 @@ set -e
 cd $BUILD_WORKSPACE_DIRECTORY
 
 PREFIX_FILE="vhdl_ls.toml.add"
+PREFIX_FILE2="vhdl_ls.toml.prefix"
+SUFFIX_FILE="vhdl_ls.toml.suffix"
 
 OUTPUT_FILE="vhdl_ls.toml"
 
@@ -12,8 +14,16 @@ bazel build //... \
   --output_groups=vhdl_ls_manifests \
   --keep_going
 
+echo "" > "${OUTPUT_FILE}"
+
 if [[ -f "${PREFIX_FILE}" ]]; then
   cp "${PREFIX_FILE}" "${OUTPUT_FILE}"
+fi
+if [[ -f "${PREFIX_FILE}" ]]; then
+  cp "${PREFIX_FILE}" "${OUTPUT_FILE}"
+fi
+if [[ -f "${PREFIX_FILE2}" ]]; then
+  cp "${PREFIX_FILE2}" "${OUTPUT_FILE}"
 fi
 
 # 3. Create the TOML header
@@ -22,5 +32,9 @@ echo "[libraries]" >> "$OUTPUT_FILE"
 # 4. Find all generated parts in bazel-bin and append them
 # Note: We look inside bazel-bin based on the current package path
 find "$(bazel info bazel-bin)" -name "*.vhdl_ls_part" -exec cat {} + >> "$OUTPUT_FILE"
+
+if [[ -f "#{SUFFIX_FILE}" ]]; then
+  cat "${SUFFIX_FILE}" >> "${OUTPUT_FILE}"
+fi
 
 # vim: filetype=bash :
